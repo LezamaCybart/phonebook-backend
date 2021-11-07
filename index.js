1c9ab85a7ea639952a6cac796f74cc38fef4cd02
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-11-07 11:33:33 -0300

    backend step2 and 3, GET single entry

--- index.js
+++ index.js
@@ -25,8 +25,24 @@ let persons = [
     }
 ]
 
+app.get("/info", (request, response) => {
+    response.send(`<h1>Phonebook has info for ${persons.length} people<h1>
+        <h1>${Date()}</h1>`)
+})
+
 app.get("/api/persons", (request, response) => {
     response.json(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) { 
+        response.json(person)
+    } else {
+        response.status(400).end()
+    }
 })
 
 const PORT = 3001;
