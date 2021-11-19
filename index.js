490f1e8df3050aafade7b97c93e337c7d2d70f75
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-11-19 11:49:04 -0300

    database step 3 and 4, delete and error handler middleware

--- index.js
+++ index.js
@@ -53,10 +53,11 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => Math.floor(Math.random() * 100)
@@ -85,6 +86,17 @@ app.post('/api/persons/', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') { 
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
